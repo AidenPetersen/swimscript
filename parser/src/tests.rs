b8ae4edd861f4fe3355e2d7d078e22728dc5cadd
Aiden Petersen <wdaidenp@iastate.edu>
2022-02-01 13:33:57 -0600

    Added unit tests for parser They are basic but ensure whitespace doesn't matter much

--- parser/src/tests.rs
+++ parser/src/tests.rs
@@ -17,3 +17,71 @@ Warm Up: {
     "#).unwrap(), BASIC_JSON)
 }
 
+#[test]
+fn no_newline() {
+    assert_eq!(to_json(r#"
+Warm Up: {
+    5 * 125 75 swim, @1:45 , 25 drill, 25 kick
+    200 IM, Drill / Swim
+    4 * 50 Choice, @:55, Variable
+}"#).unwrap(), BASIC_JSON)
+}
+
+#[test]
+fn no_newline_after_bracket() {
+    assert_eq!(to_json(r#"
+Warm Up: {
+    5 * 125 75 swim, @1:45 , 25 drill, 25 kick
+    200 IM, Drill / Swim
+    4 * 50 Choice, @:55, Variable}"#).unwrap(), BASIC_JSON)
+}
+
+#[test]
+fn no_newline_before_bracket() {
+    assert_eq!(to_json(r#"
+Warm Up: { 5 * 125 75 swim, @1:45 , 25 drill, 25 kick
+    200 IM, Drill / Swim
+    4 * 50 Choice, @:55, Variable
+}
+"#).unwrap(), BASIC_JSON)
+}
+
+#[test]
+fn newline_after_label() {
+    assert_eq!(to_json(r#"
+Warm Up:
+{
+    5 * 125 75 swim, @1:45 , 25 drill, 25 kick
+    200 IM, Drill / Swim
+    4 * 50 Choice, @:55, Variable
+}
+"#).unwrap(), BASIC_JSON)
+}
+
+
+#[test]
+fn minimum_newlines() {
+    assert_eq!(to_json(r#"
+Warm Up: {5 * 125 75 swim, @1:45 , 25 drill, 25 kick
+          200 IM, Drill / Swim
+          4 * 50 Choice, @:55, Variable}"#).unwrap(), BASIC_JSON)
+}
+
+#[test]
+fn basic_no_whitespace() {
+    assert_eq!(to_json(r#"
+Warm Up:{
+    5 * 125 75 swim,@1:45,25 drill,25 kick
+    200 IM,Drill / Swim
+    4 * 50 Choice,@:55,Variable
+}
+"#).unwrap(), BASIC_JSON)
+}
+
+#[test]
+fn basic_no_whitespace_or_newline() {
+    assert_eq!(to_json(r#"
+Warm Up:{5 * 125 75 swim,@1:45,25 drill,25 kick
+    200 IM,Drill / Swim
+    4 * 50 Choice,@:55,Variable}"#).unwrap(), BASIC_JSON)
+}
